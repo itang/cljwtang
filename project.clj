7a2a38eae32ef43ec3f36c0f3b47ca06a009f4fb
itang <live.tang@gmail.com>
2013-02-17 20:29:21 +0800

    update

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [lib-noir "0.3.5"]
+                 [lib-noir "0.3.8"]
                  [stencil "0.3.1"]
-                 [org.clojure/tools.logging "0.2.4"]]
+                 [org.clojure/tools.logging "0.2.6"]]
   :profiles {:dev {:plugins [[codox "0.6.4"]
                              [jonase/eastwood "0.0.2"]
                              [lein-localrepo "0.4.1"]
@@ -15,7 +15,7 @@
                              [lein-kibit "0.0.7"]
                              [lein-bikeshed "0.1.0"]]}
              :1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5.0 {:dependencies [[org.clojure/clojure "1.5.0-RC4"]]}}
+             :1.5.0 {:dependencies [[org.clojure/clojure "1.5.0-RC16"]]}}
    :aliases {"run-tests" ["with-profile" "1.4.0:1.5.0" "test"]}
    :warn-on-reflection true
    :injections [(require 'clojure.pprint)]

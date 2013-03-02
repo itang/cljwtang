8cc62a172b3ee38164649723f6b7e8d25d8eb217
itang <live.tang@gmail.com>
2013-03-02 09:02:10 +0800

    update clojure-1.5.0

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                              [lein-kibit "0.0.7"]
                              [lein-bikeshed "0.1.0"]]}
              :1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5.0 {:dependencies [[org.clojure/clojure "1.5.0-RC16"]]}}
+             :1.5.0 {:dependencies [[org.clojure/clojure "1.5.0"]]}}
    :aliases {"run-tests" ["with-profile" "1.4.0:1.5.0" "test"]}
    :warn-on-reflection true
    :injections [(require 'clojure.pprint)]

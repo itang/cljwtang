b7b006d3c0522af4727d18dab3cc5242a9728577
itang <live.tang@gmail.com>
2013-03-05 21:25:25 +0800

    update stencil

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [lib-noir "0.4.1"]
-                 [stencil "0.3.1"]
+                 [stencil "0.3.2"]
                  [org.clojure/tools.logging "0.2.6"]]
   :profiles {:dev {:plugins [[codox "0.6.4"]
                              [jonase/eastwood "0.0.2"]

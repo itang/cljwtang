643ea35d0ef1fa4f647cf72a7807607e3d755b3d
itang <live.tang@gmail.com>
2013-03-11 21:14:48 +0800

    update

--- src/lib_compojure/response.clj
+++ src/lib_compojure/response.clj
@@ -2,6 +2,7 @@
   (:require [noir.response :refer [content-type]]))
 
 (defn html
-  "Wraps the response with the content type for html and sets the body to the content."
+  "Wraps the response with the content type 
+   for html and sets the body to the content."
   [content]
   (content-type "text/html; charset=utf-8" content))
--- src/lib_compojure/util.clj
+++ src/lib_compojure/util.clj
@@ -1,4 +1,7 @@
-(ns lib-compojure.util)
+(ns lib-compojure.util
+  (:require [noir.request :refer [*request*]]
+            [noir.session :as session]
+            [lib-compojure.core :refer :all]))
 
 ;;@see http://blog.fnil.net/index.php/archives/27
 (defmacro defhandler
@@ -6,3 +9,11 @@
   `(defn ~name [req#]
      (let [{:keys ~args :or {~'req req#}} (:params req#)]
        ~@body)))
+
+(defmacro with-validate [validates-fn success failture]
+  `(let [ret# (~validates-fn (:params *request*))]
+     (if (empty? ret#)
+       ~success
+       (do
+         (session/flash-put! :msg (failture-message "验证错误" ret#))
+         ~failture))))

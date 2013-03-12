3e7cd1059e05135884ea1f39d238dca1115401f1
itang <live.tang@gmail.com>
2013-03-12 21:22:46 +0800

    add defhandler-with-validates

--- src/lib_compojure/util.clj
+++ src/lib_compojure/util.clj
@@ -10,10 +10,30 @@
      (let [{:keys ~args :or {~'req req#}} (:params req#)]
        ~@body)))
 
-(defmacro with-validate [validates-fn success failture]
-  `(let [ret# (~validates-fn (:params *request*))]
-     (if (empty? ret#)
-       ~success
+;;;; validate fn 规范 
+;;;;  (validate-fn (:params *request*)) => {:user ["error1"]}
+(defmacro with-validates [validates-fn success failture]
+  `(let [find# (atom  false)
+         fs# (atom ~validates-fn)
+         ret# (atom nil)
+         p# (:params *request*)]
+     (while (and (not @find#)
+                 (not (empty? @fs#)))
+       (let [f# (first @fs#)
+             r# (f# p#)]
+         (if-not (empty? r#)
+           (do (reset! find# true) (reset! ret# r#))
+           (do (reset! fs# (next @fs#))))))
+     (if-not (empty? @ret#)
        (do
-         (session/flash-put! :msg (failture-message "验证错误" ret#))
-         ~failture))))
+         (session/flash-put! :msg (failture-message "验证错误" @ret#))
+         ~failture)
+       ~success)))
+
+(defmacro defhandler-with-validates
+  [handler args validates-fn &
+   {:keys [success failture] 
+    :or {failture
+         #(throw (Exception. "validate error"))}}]
+  `(defhandler ~handler [~@args]
+     (with-validates ~validates-fn ~success ~failture)))

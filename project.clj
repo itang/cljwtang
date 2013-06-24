58e0f1e5e13133cd2e2f930e20c2ab1c5ebed155
itang <live.tang@gmail.com>
2013-06-24 20:43:56 +0800

    u

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  ;;[org.clojure/clojurescript "0.0-1586"]
                  [org.clojure/core.incubator "0.1.3"]
                  [org.clojure/core.memoize "0.5.5"]
-                 [org.clojure/core.match "0.2.0-beta3"]
+                 [org.clojure/core.match "0.2.0-rc1"]
 
                  [cljtang "0.1.1"]
                  [clj-pretty-format "0.1.1"]

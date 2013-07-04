0b5ff247c23b7e5358ad6dbbd195178e82d5e649
itang <live.tang@gmail.com>
2013-07-04 16:09:08 +0800

    add test

--- src/cljwtang/response.clj
+++ src/cljwtang/response.clj
@@ -2,11 +2,13 @@
   (:require [noir.response :refer [content-type set-headers]]))
 
 (defn html
-  "Wraps the response with the content type 
+  "Wraps the response with the content type
    for html and sets the body to the content."
   [content]
   (content-type "text/html; charset=utf-8" content))
 
 (defn content-length
-  [length content]
-  (set-headers {"Content-Length" length} content))
+  ([content]
+     (content-length (count content) content)) ;;TODO improve count
+  ([length content]
+     (set-headers {"Content-Length" length} content)))
---
+++ test/cljwtang/response_test.clj
@@ -0,0 +1,14 @@
+(ns cljwtang.response-test
+  (:use clojure.test
+        cljwtang.response))
+
+(deftest html-test
+  (is (= {:headers
+          {"Content-Type" "text/html; charset=utf-8"}, :body "ss"}
+         (html "ss"))))
+
+(deftest content-length-test
+  (is (= {:headers {"Content-Length" 2}, :body "ss"}
+         (content-length (count "ss") "ss")))
+  (is (= {:headers {"Content-Length" 2}, :body "ss"}
+         (content-length "ss"))))

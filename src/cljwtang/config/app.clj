6556a4e1c4448282bbf686cace5d4ca077b50d29
itang <live.tang@gmail.com>
2013-10-01 17:03:49 +0800

    Issue #22

--- src/cljwtang/config/app.clj
+++ src/cljwtang/config/app.clj
@@ -27,31 +27,31 @@
   "stmp 服务器配置"
   []
   (*app-config-fn* :platform.mail-server
-                 {:host "smtp.exmail.qq.com"
-                  :port 465
-                  :user "support@traup.com"
-                  :pass "traup_2012"
-                  :ssl :yes}))
+                   {:host "smtp.qq.com"
+                    :port 465
+                    :user "cljwtang@qq.com"
+                    :pass "cljwtang@2013"
+                    :ssl :yes}))
 
 (defn mail-vendors-out-rule
   "邮件服务商(规则之外的)"
   []
   (*app-config-fn* :platform.mail-vendors-out-rule
-                 {"gmail.com" "mail.google.com"
-                  "139.com" "mail.10086.cn"}))
+                   {"gmail.com" "mail.google.com"
+                    "139.com" "mail.10086.cn"}))
 
 (defn system-monitoring-mail-accounts
   "系统监控对应的邮件帐号"
   []
   (*app-config-fn* :platform.system-monitoring-mail-account
-                 ["livetang@qq.com"]))
+                   ["livetang@qq.com"]))
 
 (defn appdata-dir
   "应用存放数据的目录"
   []
   (let [file-path #(.getAbsolutePath ^java.io.File %)
         data-dir (*app-config-fn* :platform.appdata-dir
-                             (file-path (file (home) ".cljwtang/data")))]
+                                  (file-path (file (home) ".cljwtang/data")))]
     (when-not (exists? data-dir)
       (mkdirs data-dir))
     data-dir))
